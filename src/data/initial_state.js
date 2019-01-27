918e27d70bbaeb16c2caaa82f492684dc88d6436
Steven Kay <stevendkay@gmail.com>
2019-01-27 22:19:48 +0000

    moved initial state to module in /data

---
+++ src/data/initial_state.js
@@ -0,0 +1,20 @@
+const initialState = {
+  toDoItems: [{
+    id: 1,
+    text: "Learn react.js and Redux",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "????",
+    completed: true
+  },
+  {
+    id: 3,
+    text: "Profit!!",
+    completed: false
+  }
+  ]
+};
+
+export default initialState;
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,21 +1,4 @@
-const initialState = {
-  toDoItems: [{
-    id: 1,
-    text: "Learn react.js and Redux",
-    completed: false
-  },
-  {
-    id: 2,
-    text: "????",
-    completed: true
-  },
-  {
-    id: 3,
-    text: "Profit!!",
-    completed: false
-  }
-  ]
-};
+import initialState from "../data/initial_state";
 
 const todos = (state =  initialState, action) => {
   // currently just returns original state
